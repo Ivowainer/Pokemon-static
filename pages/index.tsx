881743fda64102d128f059370c1738f2859abf15
Ivowainer <ivancamposwainer@gmail.com>
2022-08-15 17:10:11 -0300

    Fetching the pokemons w/ getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api'
 import MainLayout from '../components/layouts/MainLayout'
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log(props)
+
   return (
     <MainLayout title="All Pokemons">
-      <h1>Hola Mundo</h1>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
     </MainLayout>
   )
 }
 
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get('/pokemon?limit=151');
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
 export default HomePage
